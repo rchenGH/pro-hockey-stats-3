5ddd7664035410dd2c2480f080a045a5b5bd575f
Raymond Chen <raymondchen86@hotmail.com>
2019-08-26 02:33:01 -0700

    add user model and route

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const fetch = require('node-fetch');
 // DB Config
 const db = require('./config/keys').mongoURI;
 
+// Init Middleware
+app.use(express.json({extended: false}));
 
 // Define Routes
 
@@ -21,14 +23,15 @@ app.use('/teams',  require('./routes/api/rosters'));
 app.use('/teams',  require('./routes/api/players'));
 
 mongoose
-  .connect(db, {useNewUrlParser: true})
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
-
   
 // Body parser middleware
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
 
 
 // start server
