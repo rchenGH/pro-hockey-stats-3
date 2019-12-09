ddfb9a39ae057f2e22bc4cf653c8d59ee914233b
Raymond Chen <raymondchen86@hotmail.com>
2019-12-08 16:13:43 -0800

    change send file method parameter

--- server.js
+++ server.js
@@ -35,7 +35,8 @@ if(process.env.NODE_ENV === 'production'){
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname + "/../dist/index.html"));
     if (!url.startsWith('/app/')) // since we're on local windows
     url = url.substring(1);
   res.sendFile(url);
