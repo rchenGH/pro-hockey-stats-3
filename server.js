8fd00a488edf9f81e236998e00722426e24bb350
Raymond Chen <raymondchen86@hotmail.com>
2019-12-10 15:34:18 -0800

    revert server

--- server.js
+++ server.js
@@ -30,33 +30,22 @@ mongoose.set('useCreateIndex', true);
 app.use(bodyParser.urlencoded({extended: true}));
 
 // Serve static assets in production
-// if(process.env.NODE_ENV === 'production'){
-//   // Set static folder
-//   app.use(express.static(path.join(__dirname, "client", "build")))
+if(process.env.NODE_ENV === 'production'){
+  // Set static folder
+  app.use(express.static(path.join(__dirname, "client", "build")))
 
-//   app.get('*', (req, res) => {
-//     // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-
-//     if (!url.startsWith('/app/')) // since we're on local windows
-//     url = url.substring(1);
-//   res.sendFile(url);
-//   })
-// }
-
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.join('build', 'index.html'));
-  });
+    // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+
+    if (!url.startsWith('/app/')) // since we're on local windows
+    url = url.substring(1);
+  res.sendFile(url);
+  })
 }
 
-
-// start server
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`We are live on port ${port}`)
 });
-
 
