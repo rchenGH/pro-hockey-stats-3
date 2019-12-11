ff289d380c126cbcd019c30e5f4178d3b0df7f52
Raymond Chen <raymondchen86@hotmail.com>
2019-12-10 16:06:55 -0800

    add absolute path in index

--- server.js
+++ server.js
@@ -35,7 +35,6 @@ if(process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname, "client", "build")))
 
   app.get('*', (req, res) => {
-    // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 
     if (!url.startsWith('/app/')) // since we're on local windows
