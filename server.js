2e10084d5b90e154170aa51c5ac5f6936e6aaa46
Raymond Chen <raymondchen86@hotmail.com>
2019-12-10 17:34:09 -0800

    change server to app

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 if(process.env.NODE_ENV === 'production'){
   // Set static folder
   app.use(express.static(path.join(__dirname, "client", "build")));
-  server.use('*', express.static('client/build'))
+  app.use('*', express.static('client/build'))
 
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
