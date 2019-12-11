326e362d9c76471b7c797d4780bc494eeb02ce6f
Raymond Chen <raymondchen86@hotmail.com>
2019-12-11 00:07:48 -0800

    change process env conditional

--- server.js
+++ server.js
@@ -29,19 +29,29 @@ mongoose.set('useCreateIndex', true);
 app.use(bodyParser.urlencoded({extended: true}));
 
 // Serve static assets in production
-if(process.env.NODE_ENV === 'production'){
-  // Set static folder
-  app.use(express.static(path.join(__dirname, "client", "build")));
-  app.use('*', express.static('client/build'))
+// if(process.env.NODE_ENV === 'production'){
+//   // Set static folder
+//   app.use(express.static(path.join(__dirname, "client", "build")));
+//   app.use('*', express.static('client/build'))
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+//   app.get('/team', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
   });
-
-  app.get('/teams', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+} else {
+  app.use(express.static(path.join(__dirname, '/client/public')));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
   });
 }
+
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
