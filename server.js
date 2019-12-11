38b8eef3af9370b72a84e442835a8c525dfa80d9
Raymond Chen <raymondchen86@hotmail.com>
2019-12-10 16:17:32 -0800

    change static path

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 if(process.env.NODE_ENV === 'production'){
   // Set static folder
   app.use(express.static(path.join(__dirname, "client", "build")))
+  app.use('/static', express.static(path.join(__dirname, 'public')))
 
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
