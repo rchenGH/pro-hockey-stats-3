2ae78c0eccdd89f15c365b9f0dd1b997291610f2
Raymond Chen <raymondchen86@hotmail.com>
2019-12-10 17:07:09 -0800

    add universal route

--- server.js
+++ server.js
@@ -32,7 +32,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 // Serve static assets in production
 if(process.env.NODE_ENV === 'production'){
   // Set static folder
-  app.use(express.static(path.join(__dirname, "client", "build")))
+  app.use(express.static(path.join(__dirname, "client", "build")));
+  server.use('*', express.static('client/build'))
 
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
